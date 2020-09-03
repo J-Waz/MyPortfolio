7ccc6446aa113fbacf2ba8fa2726734769a06d10
J-Waz <jgw.jakegeorge@gmail.com>
2020-09-03 12:03:19 -0500

    typewriter effect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import MyNavbar from "./components/my-navbar/my-navbar.component";
+import MyCarousel from "./components/my-carousal/my-carousal.component";
+import MyTitleMessage from "./components/title-message/title-message.component";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="App" style={{ position: "relative" }}>
       <MyNavbar />
-      My Portfolio
+      <MyTitleMessage />
+      <MyCarousel />
     </div>
   );
 };
---
+++ src/components/my-carousal/my-carousal.component.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import carousalImage1 from "../../assets/img/carousalImage1.jpg";
+import carousalImage2 from "../../assets/img/carousalImage2.jpeg";
+import carousalImage3 from "../../assets/img/carousalImage3.jpeg";
+import "./my-carousel.component.style.css"
+
+const MyCarousal = () => {
+  return (
+    <div id="home">
+      <Carousel
+        controls={false}
+        indicators
+        interval={2500}
+        pauseOnHover={false}
+      >
+        <Carousel.Item>
+          <img
+            className="d-block w- custom-img"
+            src={carousalImage1}
+            alt="First slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w- custom-img"
+            src={carousalImage3}
+            alt="Third slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w- custom-img"
+            src={carousalImage2}
+            alt="Third slide"
+          />
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+};
+
+export default MyCarousal;
